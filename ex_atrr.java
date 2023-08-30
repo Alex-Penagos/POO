cccf6f342fd52a8ddf1674e2b4fd2aa3d841fa31
Alex-Penagos <142423623+Alex-Penagos@users.noreply.github.com>
2023-08-30 10:57:28 -0500

    Add files via upload

---
+++ ex_atrr.java
@@ -0,0 +1,13 @@
+package call.unicauca.edu.co;
+
+/**
+ *
+ * @author Alex Joseph Peagos Valencia
+ */
+public class ex_atrr {
+
+    public String name = "Alex Joseph Penagos Valencia";
+    private String document = "1.137.674.231";
+    public int year_hbd = 2005;
+
+}
